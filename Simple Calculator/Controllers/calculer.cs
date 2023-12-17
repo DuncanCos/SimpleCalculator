b35144db78d50fc5762b7ccc44c3d26508f27aab
duncan costes <foxlolmen99@gmail.com>
2023-12-17 23:42:36 +0100

    ajout du projet

---
+++ Simple Calculator/Controllers/calculer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class calculer : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       public IActionResult resultat(int num1, string cal, int num2)
+        {
+            if (cal == "add")
+            {
+                ViewData["resultat"] = num1 + num2;
+            }else if (cal == "minus")
+            {
+                ViewData["resultat"] = num1 - num2;
+            }
+            else if (cal == "mult")
+            {
+                ViewData["resultat"] = num1 * num2;
+            }
+            else if (cal == "div")
+            {
+                ViewData["resultat"] = num1 / num2;
+            }
+            return View();
+        }
+    }
+}
